46bdaa421ab6ae9772aaa80b42e654b8427f4893
Julian Segura <julianl.segura@gmail.com>
2020-02-19 16:29:48 -0400

    Testing Single DB Manager

---
+++ DataAccessLayer/DataParameters.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;//se debe agregar este Using porque vamos a trabajar con datos
+
+namespace DataAccessLayer
+{
+    public class DataParameters
+    {
+        //Declaramos los parametros. Se ponen los atributos de los parametros en SQL
+
+        private string Name { get; set; }
+        private object Value { get; set; }
+        private SqlDbType DataType { get; set; }
+        private int Size { get; set; }
+        private ParameterDirection Direction { get; set; }
+
+        //Declaramos los constructores
+        //Parametros de entrada en SQL
+        public DataParameters(string _name, object _value)
+        {
+            Name = _name;
+            Value = _value;
+            Direction = ParameterDirection.Input;
+        }
+        //Parametros de salida en SQL
+
+        public DataParameters(string _name,SqlDbType _datatype,int _size)
+        {
+            Name = _name;
+            DataType = _datatype;
+            Size = _size;
+            Direction = ParameterDirection.Output;
+        }
+
+    }
+}
---
+++ DataAccessLayer/DatabaseManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DataAccessLayer
+{
+    public class DatabaseManager
+    {
+        private SqlConnection Connection = new SqlConnection("Server:");
+    }
+}
