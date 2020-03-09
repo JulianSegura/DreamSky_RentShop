990ce66a9a6d5e6cb838da74f0f4d4f5dc62cb3e
Inouky <francisc80@gmail.com>
2020-03-08 21:14:26 -0400

    Update Splash

--- DreamSkyEntities/clsRol.cs
+++ DreamSkyEntities/clsRol.cs
@@ -6,10 +6,24 @@ namespace DreamSkyEntities
 {
     public class clsRol
     {
-        public int Id { get;}
+        public int Id { get; set; }
         public string Nombre { get; set; }
         private int limiteEmpleados { get; set; }
         private int limiteEnDemanda { get; set; }
         public bool Activo { get; set; }
+        public List<clsPermiso> Permisos { get; set; }
+
+        public clsRol(int rolId)
+        {
+            Id = rolId;
+
+        }
+
+        public clsRol()
+        {
+
+        }
+
+        private void get
     }
 }
